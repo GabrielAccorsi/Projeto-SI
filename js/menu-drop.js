ccd2c91fb1cdc90975fc9d0c0cab46c8bb8c7a58
Kaiquy Gabriel <gabrielpaccorsi@gmail.com>
2024-09-18 12:33:34 -0300

    arrumando coisinhas

--- js/menu-drop.js
+++ js/menu-drop.js
@@ -3,7 +3,6 @@ function logout() {
     location.reload();
 }
 
-
 function menu() {
     const usuarioLogado = JSON.parse(localStorage.getItem('usuarioLogado'));
     const profileDropdownList = document.querySelector(".profile-dropdown-list");
@@ -11,10 +10,11 @@ function menu() {
     const nomeEntrar = document.getElementById("nome_entrar");
     const profileImg = btn.querySelector('.profile-img');
 
-    const toggle = () => profileDropdownList.classList.toggle("active");
+    const toggle = () => {
+        profileDropdownList.classList.toggle("active");
+    };
 
     if (usuarioLogado) {
-        
         btn.addEventListener('click', toggle);
 
         window.addEventListener('click', function (e) {
@@ -23,21 +23,42 @@ function menu() {
             }
         });
 
-        
         profileImg.style.backgroundImage = `url('${usuarioLogado.foto}')`;
         nomeEntrar.innerHTML = `${usuarioLogado.nome} <i class="fa-solid fa-angle-down"></i>`;
 
+        if (usuarioLogado.tipo === 'aluno') {  
+            document.querySelector('#Perfil').addEventListener('click', function(event) {
+            event.preventDefault(); 
+            window.location.href = 'pagina_aluno.html';
+          });
+  
+        } else if (usuarioLogado.tipo === 'adm') {
+            document.querySelector('#Perfil').addEventListener('click', function(event) {
+                event.preventDefault(); 
+                window.location.href = 'pagina_adm.html'; 
+              });
+        }
+        document.querySelector('#Posts').addEventListener('click', function(event) {
+            event.preventDefault(); 
+            window.location.href = 'forum/forums.html'; 
+          });
         
-        const exitItem = profileDropdownList.querySelector('#Deslogar');
+
+        const exitItem = document.getElementById('Deslogar');
         exitItem.addEventListener('click', logout);
     } else {
-        
         profileImg.style.backgroundImage = `url('../imagens/entrar.png')`;
-        nomeEntrar.innerHTML = `Entrar `;
+        nomeEntrar.innerHTML = `Entrar`;
 
-        btn.addEventListener('click', function() {
+        btn.addEventListener('click', function () {
             window.location.href = 'login.html';
         });
     }
 }
+
 document.addEventListener('DOMContentLoaded', menu);
+const toggle = () => {
+    console.log("Clique detectado no menu!");
+    profileDropdownList.classList.toggle("active");
+};
+
